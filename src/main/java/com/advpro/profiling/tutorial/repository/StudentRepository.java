b815217ddfe6255cd75525ae6ce0e3c37e40e0fd
Vincent-Davis <leonardvincent62@gmail.com>
2025-03-14 09:33:03 +0700

    [Refactoring] - Optimize findStudentWithHighestGpa using repository query to reduce data load

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -1,12 +1,15 @@
 package com.advpro.profiling.tutorial.repository;
 
-import com.advpro.profiling.tutorial.model.Student;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.advpro.profiling.tutorial.model.Student;
 /**
  * @author muhammad.khadafi
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findTopByOrderByGpaDesc();
 }
